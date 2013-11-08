6e17978f6c1dfe5909b3806194f7ac8137bd70cb
zhang hailong <hailongz.hit@gmail.com>
2013-11-08 11:21:27 +0800

    config

--- org.hailong.workspace.admin.tag/config.php
+++ org.hailong.workspace.admin.tag/config.php
@@ -5,10 +5,10 @@ return array(
 	"title" => "标签管理",
 	"group" => "基础服务",	
 	"icon" => "",
-	//"url" => "../org.hailong.workspace.admin.tag/index.php",
-	//"url-active" => "../org.hailong.workspace.admin.tag/active.php",
-	"url" => "workspace/admin/tag/index.php",
-	"url-active" => "workspace/admin/tag/active.php",
+	"url" => "../org.hailong.workspace.admin.tag/index.php",
+	"url-active" => "../org.hailong.workspace.admin.tag/active.php",
+// 	"url" => "workspace/admin/tag/index.php",
+// 	"url-active" => "workspace/admin/tag/active.php",
 	"auth-alias" => "workspace/admin/tag",
 );
 
