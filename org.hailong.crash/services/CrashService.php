b3b7eedb6f6ca7db00d4403e81970243afa35a89
zhang hailong <hailongz.hit@gmail.com>
2013-11-15 18:17:07 +0800

    update crash

--- org.hailong.crash/services/CrashService.php
+++ org.hailong.crash/services/CrashService.php
@@ -14,27 +14,31 @@ class CrashService extends Service{
 			$context = $this->getContext();
 			$dbContext = $context->dbContext(DB_CRASH);
 			
-			$item = new DBCrash();
+			$identifier = trim($task->identifier);
 			
-			$item->identifier = $task->identifier;
-			$item->version = $task->version;
-			$item->build = $task->build;
-			$item->systemName = $task->systemName;
-			$item->systemVersion = $task->systemVersion;
-			$item->model = $task->model;
-			$item->deviceName = $task->deviceName;
-			$item->deviceIdentifier = $task->deviceIdentifier;
-			
-			if(is_string($task->exception)){
-				$item->exception = $task->exception;
+			if($identifier){
+				$item = new DBCrash();
+				
+				$item->identifier = $task->identifier;
+				$item->version = $task->version;
+				$item->build = $task->build;
+				$item->systemName = $task->systemName;
+				$item->systemVersion = $task->systemVersion;
+				$item->model = $task->model;
+				$item->deviceName = $task->deviceName;
+				$item->deviceIdentifier = $task->deviceIdentifier;
+				
+				if(is_string($task->exception)){
+					$item->exception = $task->exception;
+				}
+				else if($task->exception){
+					$item->exception = json_encode($task->exception);
+				}
+				
+				$item->createTime = time();
+				
+				$dbContext->insert($item);
 			}
-			else if($task->exception){
-				$item->exception = json_encode($task->exception);
-			}
-			
-			$item->createTime = time();
-			
-			$dbContext->insert($item);
 			
 			return false;
 		}
