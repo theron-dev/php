80d6790dea5b57b5b24c861f08fc522931bd4bc0
zhang hailong <hailongz.hit@gmail.com>
2013-11-30 13:32:45 +0800

    update coin admin

--- org.hailong.workspace.admin.coin/config.php
+++ org.hailong.workspace.admin.coin/config.php
@@ -5,10 +5,10 @@ return array(
 	"title" => "金币记录",
 	"group" => "基础服务",
 	"icon" => "",
-	//"url" => "../org.hailong.workspace.admin.asynctask/index.php",
-	//"url-active" => "../org.hailong.workspace.admin.asynctask/active.php",
-	"url" => "workspace/admin/coin/index.php",
-	"url-active" => "workspace/admin/coin/active.php",
+	"url" => "../org.hailong.workspace.admin.coin/index.php",
+	"url-active" => "../org.hailong.workspace.admin.coin/active.php",
+	//"url" => "workspace/admin/coin/index.php",
+	//"url-active" => "workspace/admin/coin/active.php",
 	"auth-alias" => "workspace/admin/coin",
 );
 
