523fd83fb08057c842ab5ac1bc0bf8e2c629ab30
zhang hailong <hailongz.hit@gmail.com>
2014-02-11 14:27:25 +0800

    update goods

--- org.hailong.goods/services/GoodsService.php
+++ org.hailong.goods/services/GoodsService.php
@@ -567,8 +567,6 @@ class GoodsService extends Service{
 		$httpCode = curl_getinfo($curl, CURLINFO_HTTP_CODE);
 		$contentType = curl_getinfo($curl, CURLINFO_CONTENT_TYPE);
 		
-		var_dump($htmlSource);
-		
 		if($htmlSource && $httpCode == 200){
 		
 			$html = new DOMDocument();
@@ -648,7 +646,7 @@ class GoodsService extends Service{
 					$task->source = $source;
 					$task->url = $url;
 					$task->wapUrl = $url;
-					
+					var_dump($task);
 					$context->handle("GoodsCreateTask",$task);
 					
 					return $task->results;
