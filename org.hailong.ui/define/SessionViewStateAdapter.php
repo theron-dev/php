ff08378cefa5eab9618f1b11b76aadacd51e0a0e
zhang hailong <hailongz.hit@gmail.com>
2014-02-12 11:12:09 +0800

    update ui

--- org.hailong.ui/define/SessionViewStateAdapter.php
+++ org.hailong.ui/define/SessionViewStateAdapter.php
@@ -4,48 +4,16 @@ class SessionViewStateAdapter implements IViewStateAdapter{
 	
 	private $alias;
 	
-	private $viewState;
-	
 	public function __construct($alias = ""){
 		$this->alias = $alias;
-		$this->viewState = false;
-	}
-	
-	public function __destruct(){
-		
-		if($this->viewState){
-			global $UI_SESSION_DIR;
-			$sessionId = session_id();
-			
-			$data = base64_encode(gzcompress( serialize($this->viewState) ,9) );
-		
-			file_put_contents($UI_SESSION_DIR.'/'.$sessionId.'_'.md5($this->alias).'.vst', $data);
-		}
 	}
 	
 	public function saveViewState($data){
-		$this->viewState = $data;
+		$_SESSION["view/state/".$this->alias] = $data;
 	}
 	
 	public function loadViewState(){
-		
-		if($this->viewState === false){
-			
-			global $UI_SESSION_DIR;
-			
-			$sessionId = session_id();
-			
-			$path = $UI_SESSION_DIR.'/'.$sessionId.'_'.md5($this->alias).'.vst';
-			
-			if(file_exists($path) && ($data = file_get_contents($path))){
-				$this->viewState = unserialize( gzuncompress(base64_decode($data)));
-			}
-			else{
-				$this->viewState = null;
-			}
-		}
-		
-		return $this->viewState;
+		return isset($_SESSION["view/state/".$this->alias]) ? $_SESSION["view/state/".$this->alias] : null;
 	}
 	
 	public function setContext($context){
