7ffd3b476e1c10c51f5873fcf314b83eeb4390c6
zhang hailong <hailongz.hit@gmail.com>
2014-03-09 13:37:24 +0800

    update feedback

--- org.hailong.workspace.admin.feedback/controllers/FeedbackSearchController.php
+++ org.hailong.workspace.admin.feedback/controllers/FeedbackSearchController.php
@@ -89,7 +89,7 @@ class FeedbackSearchController extends ViewController{
 	
 		$offset = ($pageIndex -1) *  $this->pageSize;
 	
-		$rs = $dbContext->queryEntitys("DBFeedback",$sql." ORDER BY cid DESC LIMIT {$offset},{$this->pageSize}");
+		$rs = $dbContext->queryEntitys("DBFeedback",$sql." ORDER BY fid DESC LIMIT {$offset},{$this->pageSize}");
 	
 		if($rs){
 	
