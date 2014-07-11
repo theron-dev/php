82c090fcff12835dd87e3f0be325e293168bce96
zhang hailong <hailongz.hit@gmail.com>
2014-07-11 11:22:08 +0800

    update

--- org.hailong.amazon/amazon.php
+++ org.hailong.amazon/amazon.php
@@ -2,6 +2,8 @@
 
 global $library;
 
+require_once "$library/org.hailong.amazon/vendor/autoload.php";
+
 require_once "$library/org.hailong.amazon/tasks/AmazonTask.php";
 require_once "$library/org.hailong.amazon/tasks/AmazonSearchTask.php";
 require_once "$library/org.hailong.amazon/tasks/AmazonLookupTask.php";
