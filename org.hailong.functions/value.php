e33e007025c98d9b847245a53959129f172570d0
zhang hailong <hailongz.hit@gmail.com>
2014-07-14 13:37:47 +0800

    update

--- org.hailong.functions/value.php
+++ org.hailong.functions/value.php
@@ -1,5 +1,33 @@
 <?php
 
+function ValueForKeys($object,$keys,$defaultValue=null){
+	
+	if(!is_array($keys)){
+		$keys = preg_split("/\\./i", $keys);
+	}
+	
+	$c = count($keys);
+	$index = 0;
+	$v = $object;
+	
+	while($index < $c){
+		
+		$v = Value($v,$keys[$index],null);
+		
+		if($v === null){
+			break;
+		}
+		
+		$index ++;
+	}
+	
+	if($v === null){
+		return $defaultValue;
+	}
+	
+	return $v;
+}
+
 function Value($object,$key,$defaultValue=null){
 	
 	if(is_object($object) && isset($object->$key)){
@@ -13,6 +41,14 @@ function Value($object,$key,$defaultValue=null){
 	return $defaultValue;
 }
 
+function StringValueForKeys($object,$keys,$defaultValue=''){
+	$v = ValueForKeys($object,$keys,null);
+	if($v=== null){
+		return $defaultValue;
+	}
+	return ''.$v;
+}
+
 function StringValue($object,$key,$defaultValue=''){
 	$v = Value($object,$key,null);
 	if($v=== null){
@@ -21,12 +57,28 @@ function StringValue($object,$key,$defaultValue=''){
 	return ''.$v;
 }
 
+function IntValueForKeys($object,$keys,$defaultValue=0){
+	$v = ValueForKeys($object,$keys,null);
+	if($v=== null){
+		return $defaultValue;
+	}
+	return intval($v);
+}
+
 function IntValue($object,$key,$defaultValue=0){
 	$v = Value($object,$key,null);
 	if($v=== null){
 		return $defaultValue;
 	}
 	return intval($v);
+}
+
+function DoubleValue($object,$keys,$defaultValue=0){
+	$v = ValueForKeys($object,$keys,null);
+	if($v=== null){
+		return $defaultValue;
+	}
+	return doubleval($v);
 }
 
 function DoubleValue($object,$key,$defaultValue=0){
